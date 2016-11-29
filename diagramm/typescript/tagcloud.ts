84ed5e21760e966364d70190fcfe0b6b193d3dae
mls <mlslisa105@hotmail.com>
2016-11-30 00:30:37 +0100

    tagcloud

--- diagramm/typescript/tagcloud.ts
+++ diagramm/typescript/tagcloud.ts
@@ -431,28 +431,23 @@ module Chart{
                     .start();
             });
 
-            var color = d3.scale.linear()
-                .domain([0,1,2,3,4,5,6,10,15,20,100])
-                .range(["#ddd", "#ccc", "#bbb", "#aaa", "#999", "#888", "#777", "#666", "#555", "#444", "#333", "#222"]);
-
             function draw(words) {
                 d3.select("body").append("svg")
-                    .attr("width", 850)
-                    .attr("height", 350)
-                    .attr("class", "wordcloud")
+                    .attr("width", 500)
+                    .attr("height", 500)
                     .append("g")
-                    // without the transform, words words would get cutoff to the left and top, they would
-                    // appear outside of the SVG area
-                    .attr("transform", "translate(320,200)")
+                    .attr("transform", "translate(150,150)")
                     .selectAll("text")
                     .data(words)
                     .enter().append("text")
-                    .style("font-size", function(d:any) { return d.size + "px"; })
-                    .style("fill", function(d, i) { return color(i); })
-                    .attr("transform", function(d:any) {
+                    .style("font-size", function(d) { return d.size + "px"; })
+                    .style("font-family", "Impact")
+                    .style("fill", function(d, i) { return fill(i); })
+                    .attr("text-anchor", "middle")
+                    .attr("transform", function(d) {
                         return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
                     })
-                    .text(function(d:any) { return d.text; });
+                    .text(function(d) { return d.text; });
             }
         }
     }
